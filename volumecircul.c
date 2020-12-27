690834c7e39f93e1d306dc5576917df0499679f6
Aartichoudhary41 <74286612+Aartichoudhary41@users.noreply.github.com>
2020-12-27 20:15:40 +0530

    Add files via upload

---
+++ volumecircul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float r;
+	printf("area of circul ");
+	scanf("%f",&r);
+	printf("arae of circul is %f \n",3.14*r*r);
+	printf("volume of circul");
+	scanf("%f",&r);
+	printf("volume of circul is %f",1.333*3.14*r*r);
+}
+
